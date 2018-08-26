b638c7ee45451ddeb0cbe56ec0459f57f4b871c4
rogermutt <r1ger_pg@hotmail.com>
2018-08-26 10:02:49 +1000

    Installed Webpack + Babel + Dev Dep

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"
+var React = require("react");
+var ReactDOM = require("react-dom");
+require("./index.css");
 
 
-class app extends React.Component {
+class App extends React.Component {
     render () {
         return (
-
             <div>Hello World</div>
         );
     }
 }
+
+ReactDOM.render( <App/>, document.getElementById("app") );
